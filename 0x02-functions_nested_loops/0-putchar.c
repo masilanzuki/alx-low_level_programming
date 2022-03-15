273bf434c20ca9ab6f2ea2f093594c58fdc4ee41
masilanzuki <massilaeugene@gmail.com>
2022-03-15 02:36:08 -0700

    updated 0 file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
- int main(void)
-{
-  printf("_putchar\n");
 
-  return (0);
+int main(void)
+{
+	int i = 1;
+	char name[8] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(name[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
