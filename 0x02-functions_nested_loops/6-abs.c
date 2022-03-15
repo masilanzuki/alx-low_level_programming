2002c7b4aaae5c2c4a596ac7536b4f1b2bb8cea1
masilanzuki <massilaeugene@gmail.com>
2022-03-15 07:08:33 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,15 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- *_abs - wirtes the absolute value of a number.
- *@n: the number to be written.
- *Return: (abs (n)).
+ * _abs - write the absolute value of an number .
+ * @n: the number to be tested.
+ * Return: (abs (n)).
  */
-int _abs(int n)
+int  _abs(int n)
 {
-  if (n < 0)
-    {
-      n = n * -1;
-    }
-  return (n);
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+return (n);
 }
