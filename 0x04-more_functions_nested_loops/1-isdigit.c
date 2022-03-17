d5a610035493f04596de9e9b6f99e23a658bc6e0
masilanzuki <massilaeugene@gmail.com>
2022-03-17 02:39:40 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _isdigit - define if a variable is a digit.
+ *
+ * @c: integer to be tested.
+ * Return: 1 if true .
+ */
+int _isdigit(int c)
+{
+if ((c >= 48) && (c <= 57))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
