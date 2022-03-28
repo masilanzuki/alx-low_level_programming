213b69a381ebba0b081c2012ed1657b61a76ff32
masilanzuki <massilaeugene@gmail.com>
2022-03-28 11:00:20 +0300

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locates c in string s.
+ * @s: the string
+ * @c: the character to locate in the string.
+ * Return: character c if true.
+ * Return: 0 if false.
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != 0)
+{
+if (s[i] == c)
+break;
+i++;
+}
+if (s[i] == c)
+return (s + 2);
+return (0);
+}
