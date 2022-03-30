1c5c3146a381c11c00c6857522a1dc4392bc010e
masilanzuki <massilaeugene@gmail.com>
2022-03-30 10:45:41 +0300

    task 0 added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts_recursion - prints a string.
+ *@s: string to print.
+ */
+void _puts_recursion(char *s)
+{
+int i = 0;
+if (s[i])
+{
+_putchar(s[i]);
+i++;
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
+}
