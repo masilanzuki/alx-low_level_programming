e59465ed217661bd688a9aa0a5f77293e48f340a
masilanzuki <massilaeugene@gmail.com>
2022-04-11 18:23:47 +0300

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,41 +2,51 @@
 #include <stdlib.h>
 
 /**
- * copy -copy a string.
- * @x: string address (destination).
- * @y: string address (source).
- * Return: string address.
- */
-
+ * copy - Copy a string.
+ * @x: String address (destination).
+ * @y: String address (source).
+ * Return: String address.
+ **/
 char *copy(char *x, char *y)
 {
-int i;
-for (i = 0; y[i] != '\0'; i++)
-x[i] = y[i];
-x[i] = '\0';
-return (x);
+	int i;
+
+	for (i = 0; y[i] != '\0'; i++)
+		x[i] = y[i];
+	x[i] = '\0';
+	return (x);
 }
 /**
- * new_dog - create new dog.
- * @name:string address (dog name).
- * @age: float (dog age).
- * @owner: string address (dog owner).
- * Return: address of new string.
- */
-dog_t *new_dog(char *name, float *age, char *owner)
+ * new_dog - Create a new dog.
+ * @name: String address (dog name).
+ * @age: Float (dog age).
+ * @owner: String address (dog owner).
+ * Return: Address of new dog.
+ **/
+
+dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *str;
-str = malloc(sizeof(dog_t));
-if (str == NULL)
-return (NULL);
-str->name = malloc(sizeof(name) + 1);
-if (str->name == NULL)
-{
-free(str->name);
-free(str);
-}
-str->age = *age;
-str->name = (copy(str->name, name));
-str->owner = (copy(str->owner, owner));
-return (str);
+	dog_t *str;
+
+	str = malloc(sizeof(dog_t));
+	if (str == NULL)
+		return (NULL);
+
+	str->name = malloc(sizeof(name) + 1);
+	if (str->name == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+
+	str->owner = malloc(sizeof(owner) + 1);
+	if (str->owner == NULL)
+	{
+		free(str->name);
+		free(str);
+	}
+	str->age = age;
+	str->name = (copy(str->name, name));
+	str->owner = (copy(str->owner, owner));
+	return (str);
 }
