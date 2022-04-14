53056be0d12cf9511840d715532bcdc2c51af174
masilanzuki <massilaeugene@gmail.com>
2022-04-14 13:22:15 +0300

    task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print-stirngs - print string parameters.
+ * @separator: string that contains the separator btwn stings.
+ * @n: number of strings.
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i = 0;
+char *s;
+
+va_start(args, n);
+for (; i < n; i++)
+{
+s = va_arg(args, char *);
+if (s == NULL)
+printf("(nil)");
+else
+printf("%s",s);
+if (separator && i < n - 1)
+printf("%s", separator);
+}
+printf("\n");
+va_end(args);
+}
